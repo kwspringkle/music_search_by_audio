40c062d004548c826eac3795d66fcec213d0f00c
Quynh <101940232+kwspringkle@users.noreply.github.com>
2024-09-05 22:23:48 +0700

    record function

--- music_search/urls.py
+++ music_search/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('upload.urls'))
+    path('', include('upload.urls')),
+    path('', include('record.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
--- record/views.py
+++ record/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import os
+from .models import recorded_audio
+from django.urls import reverse
 from upload.views import search
-
 # Create your views here.
 
+API_KEY = os.environ['API_KEY']
+
+def record_audio_file(request):
+    if request.method == "POST":
+        recorded_audio_name = request.POST.get('recorded_audio_name')
+        audio_file = request.FILES['audio_file']
+
+        recorded_audio_obj = recorded_audio(
+            recorded_audio_name=recorded_audio_name,
+            path=audio_file
+        )
+        recorded_audio_obj.save()
+        audio_path = f"media/recorded_audio/{audio_file}"
+        return redirect(reverse('search', kwargs={'audio_path': audio_path}))
+
+    return render(request, 'index.html')
